29caadc1b89533a7127582bfe82a708daa95d59b
eduardsui <eduard.suica@gmail.com>
2024-02-04 21:46:01 +0200

    updated

---
+++ modules/standard.lib.piper/src/include/piper-phonemize/tashkeel.hpp
@@ -0,0 +1,39 @@
+#ifndef TASHKEEL_H_
+#define TASHKEEL_H_
+
+#include <map>
+#include <set>
+#include <string>
+
+#include <onnxruntime_cxx_api.h>
+
+#include "shared.hpp"
+
+// https://github.com/mush42/libtashkeel
+namespace tashkeel {
+
+const std::string instanceName = "piper_tashkeel";
+const int PAD_ID = 0;
+const int UNK_ID = 1;
+const std::size_t MAX_INPUT_CHARS = 315;
+
+extern std::set<char32_t> HARAKAT_CHARS;
+extern std::set<int> INVALID_HARAKA_IDS;
+extern std::map<char32_t, int> inputVocab;
+extern std::map<int, std::vector<char32_t>> outputVocab;
+
+struct State {
+  Ort::Session onnx;
+  Ort::AllocatorWithDefaultOptions allocator;
+  Ort::SessionOptions options;
+  Ort::Env env;
+
+  State() : onnx(nullptr){};
+};
+
+PIPERPHONEMIZE_EXPORT void tashkeel_load(std::string modelPath, State &state);
+PIPERPHONEMIZE_EXPORT std::string tashkeel_run(std::string text, State &state);
+
+} // namespace tashkeel
+
+#endif // TASHKEEL_H_
---
+++ modules/standard.lib.piper/src/main.cpp
@@ -0,0 +1,74 @@
+//------------ standard header -----------------------------------//
+#include "stdlibrary.h"
+//------------ end of standard header ----------------------------//
+#include "library.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "piper.hpp"
+
+struct PiperContext {
+    struct piper::PiperConfig piper_config;
+    struct piper::Voice voice;
+    piper::SpeakerId speaker;
+};
+
+//=====================================================================================//
+CONCEPT_DLL_API ON_CREATE_CONTEXT MANAGEMENT_PARAMETERS {
+    return 0;
+}
+//=====================================================================================//
+CONCEPT_DLL_API ON_DESTROY_CONTEXT MANAGEMENT_PARAMETERS {
+    return 0;
+}
+//=====================================================================================//
+CONCEPT_FUNCTION_IMPL(PiperInit, 2)
+    T_STRING(PiperInit, 0);
+    T_STRING(PiperInit, 1);
+
+    struct PiperContext *handle = new PiperContext;
+    if (handle) {
+        try {
+            piper::initialize(handle->piper_config);
+            std::optional<piper::SpeakerId> speaker_id;
+            piper::loadVoice(handle->piper_config, PARAM(0), PARAM(1), handle->voice, speaker_id, false);
+       } catch (...) {
+            piper::terminate(handle->piper_config);
+            delete handle;
+            handle = NULL;
+       }
+    }
+    RETURN_NUMBER((SYS_INT)handle);
+END_IMPL
+//=====================================================================================//
+CONCEPT_FUNCTION_IMPL(PiperTTS, 2)
+    T_HANDLE(PiperTTS, 0);
+    T_STRING(PiperTTS, 1);
+
+    std::vector<int16_t> audioBuffer;
+    piper::SynthesisResult result;
+
+    struct PiperContext *handle = (struct PiperContext *)(SYS_INT)PARAM(0);
+    textToAudio(handle->piper_config, handle->voice, PARAM(1), audioBuffer, result, NULL);
+
+    if (audioBuffer.size() > 0) {
+        RETURN_BUFFER((const char *)audioBuffer.data(), audioBuffer.size() * 2);
+    } else {
+        RETURN_STRING("");
+    }
+END_IMPL
+//=====================================================================================//
+CONCEPT_FUNCTION_IMPL(PiperDone, 1)
+    T_NUMBER(PiperDone, 0);
+
+    struct PiperContext *handle = (struct PiperContext *)(SYS_INT)PARAM(0);
+    if (handle) {
+        piper::terminate(handle->piper_config);
+        delete handle;
+        SET_NUMBER(0, 0);
+    }
+    RETURN_NUMBER(0);
+END_IMPL
+//=====================================================================================//
