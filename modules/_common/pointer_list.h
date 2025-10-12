315ce25f8a29e55a8b6aeb6455f5098618a9a4da
eduardsui <eduard.suica@gmail.com>
2025-10-12 21:08:35 +0300

    updated

--- modules/_common/pointer_list.h
+++ modules/_common/pointer_list.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <map>
+#include <atomic>
 
 #ifndef INT_POINTER_TYPE
 	#define INT_POINTER_TYPE	unsigned int
@@ -31,7 +32,9 @@ public:
 
 struct pointer_list {
 	std::map<INT_POINTER_TYPE, pointer_list_object> map;
+	std::atomic_flag atomic_flag = ATOMIC_FLAG_INIT;
 	INT_POINTER_TYPE index;
+	INT_POINTER_TYPE reuse_index;
 };
 
 void pointerList_init(struct pointer_list *ptr_list) {
@@ -40,17 +43,27 @@ void pointerList_init(struct pointer_list *ptr_list) {
 		return;
 
 	ptr_list->index = 0;
+	ptr_list->reuse_index = 0;
 }
 
 INT_POINTER_TYPE pointerList_add(struct pointer_list *ptr_list, const void *ptr, const void *context) {
-
 	if ((!ptr_list) || (!ptr))
 		return 0;
 
-	++ ptr_list->index;
-	pointer_list_object ptr_2(ptr, context, ptr_list->index);
-	ptr_list->map[ptr_list->index] = ptr_2;
-	return ptr_list->index;
+	while (ptr_list->atomic_flag.test_and_set(std::memory_order_acquire)) { };
+
+	INT_POINTER_TYPE index = ptr_list->reuse_index;
+	if (index) {
+		ptr_list->reuse_index = 0;
+	} else {
+		index = ++ ptr_list->index;
+	}
+	pointer_list_object ptr_2(ptr, context, index);
+	ptr_list->map[index] = ptr_2;
+
+	ptr_list->atomic_flag.clear(std::memory_order_release);
+
+	return index;
 }
 
 void *pointerList_pointer(struct pointer_list *ptr_list, INT_POINTER_TYPE ptr_id, const void *context) {
@@ -58,7 +71,10 @@ void *pointerList_pointer(struct pointer_list *ptr_list, INT_POINTER_TYPE ptr_id
 	if ((!ptr_id) || (!ptr_list))
 		return (void *)0;
 
+	while (ptr_list->atomic_flag.test_and_set(std::memory_order_acquire)) { };
 	pointer_list_object ptr_2 = ptr_list->map[ptr_id];
+	ptr_list->atomic_flag.clear(std::memory_order_release);
+
 	if (ptr_2.context != context)
 		return (void *)0;
 
@@ -69,14 +85,22 @@ void *pointerList_free(struct pointer_list *ptr_list, INT_POINTER_TYPE ptr_id, c
 	if ((!ptr_id) || (!ptr_list))
 		return (void *)0;
 
+	while (ptr_list->atomic_flag.test_and_set(std::memory_order_acquire)) { };
+
 	pointer_list_object ptr_2 = ptr_list->map[ptr_id];
-	if (ptr_2.context != context)
+	if (ptr_2.context != context) {
+		ptr_list->atomic_flag.clear(std::memory_order_release);
 		return (void *)0;
+	}
 
 	ptr_list->map.erase(ptr_id);
 
 	if (ptr_id == ptr_list->index)
 		ptr_list->index --;
+	else
+		ptr_list->reuse_index = ptr_id;
+
+	ptr_list->atomic_flag.clear(std::memory_order_release);
 
 	return (void *)ptr_2.ptr;
 }
@@ -85,7 +109,9 @@ void pointerList_deinit(struct pointer_list *ptr_list) {
 	if (!ptr_list)
 		return;
 
+	while (ptr_list->atomic_flag.test_and_set(std::memory_order_acquire)) { };
 	ptr_list->map.clear();
+	ptr_list->atomic_flag.clear(std::memory_order_release);
 }
 
 #endif
